65cfab33734ff056187035f139dfa7e303da71de
pjm605 <pjm605@gmail.com>
2017-10-01 01:37:01 +0000

    implement transfer, and allow logic

--- contracts/FixedToken.sol
+++ contracts/FixedToken.sol
@@ -6,14 +6,18 @@ import "./Owned.sol";
 
 contract FixedToken is ERC20TokenI, Owned
 {
+	// symbol and name
+	string public constant symbol = "FixedToken";
+	string public constant name = "Fixed Token Example";
 
 	// initial number of tokens
 	uint public totalTokens = 1000000;
 
-
 	// address to balance mapping
 	mapping(address => uint) balances;
 
+	// allow others to transfer from another's account'
+	mapping(address => mapping(address => uint)) allowed;
 	function FixedToken()
 	{
 		balances[msg.sender] = totalTokens;
@@ -21,6 +25,7 @@ contract FixedToken is ERC20TokenI, Owned
 
 	// ERC20TokenI functions
 	function totalSupply()
+		public
 		constant
 		returns (uint totalSupply)
 	{
@@ -28,16 +33,57 @@ contract FixedToken is ERC20TokenI, Owned
 	}
 
 	function balanceOf(address _owner)
+		public
 		constant
 		returns (uint balance)
 	{
 		balance = balances[_owner];
 	}
 
-	function transfer(address _to, uint _value) returns (bool success);
-	function transferFrom(address _from, address _to, uint _value) returns (bool success);
-	function approve(address _spender, uint _value) returns (bool success);
-	function allowance(address _owner, address _spender) constant returns (uint remaining);
+	function transfer(address _to, uint _value)
+		public
+		returns (bool success)
+	{
+		require(_to != address(0));
+		require(_value > 0);
+		require(balances[msg.sender] >= _value);
+		// handle overflow
+		require(balances[_to] + _value > balances[_to]);
+
+		balances[msg.sender] -= _value;
+		balances[_to] += _value;
+		Transfer(msg.sender, _to, _value);
+		return true;
+	}
+
+	function transferFrom(address _from, address _to, uint _value)
+		public	
+		returns (bool success)
+	{
+		require(_from != address(0));
+		require(_to != address(0));
+
+		// TODO
+		return true;
+	}
+
+
+	// the owner of the contract can allow others to call transferFrom the owner's account
+	function approve(address _spender, uint _value)
+		public
+		fromOwner()
+		returns (bool success)
+	{
+		allowed[msg.sender][_spender] = _value;
+		return true;
+	}
+
+	function allowance(address _owner, address _spender)
+		constant
+		returns (uint remaining)
+	{
+		remaining = allowed[_owner][_spender];
+	}
 
 
 }
